c49805e141be49fb749c0815b734af5f3fdb5c38
Mr.Kang <1293557070@qq.com>
2020-04-03 10:45:58 +0800

    测试，搭建springboot

---
+++ src/main/java/controller/test.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
